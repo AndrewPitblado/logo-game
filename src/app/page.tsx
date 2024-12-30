5d40b22a89ef3b187e1d655cf08a8ecdb1c240ed
Andrew Scott Pitblado <apitblado@gmail.com>
2024-12-30 10:41:52 -0500

    Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import { db } from "~/server/db";
 const mockUrls = [
   "https://img.logo.dev/spotify.com?token=pk_G5936h44REiL0bO_Jbt2-w&size=140&format=png&retina=true",
   "https://img.logo.dev/netflix.com?token=pk_G5936h44REiL0bO_Jbt2-w&size=140&format=png&retina=true",
@@ -12,9 +12,13 @@ const mockLogos = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main className="flex min-h-screen flex-wrap items-center justify-center">
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
       {mockLogos.map((logo) => (
         <div key={logo.id} className="m-4">
           <img src={logo.url} alt={`Logo ${logo.id}`} />
