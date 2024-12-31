610303c4b50357cae7a414ba724531e14d556796
Andrew Scott Pitblado <apitblado@gmail.com>
2024-12-30 19:33:53 -0500

    Actually linking database to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,14 @@
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  "https://img.logo.dev/spotify.com?token=pk_G5936h44REiL0bO_Jbt2-w&size=140&format=png&retina=true",
-  "https://img.logo.dev/netflix.com?token=pk_G5936h44REiL0bO_Jbt2-w&size=140&format=png&retina=true",
-  "https://img.logo.dev/instagram.com?token=pk_G5936h44REiL0bO_Jbt2-w&size=140&format=png&retina=true",
-  "https://img.logo.dev/facebook.com?token=pk_G5936h44REiL0bO_Jbt2-w&size=140&format=png&retina=true",
-];
-
-const mockLogos = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
   return (
     <main className="flex min-h-screen flex-wrap items-center justify-center">
-      {posts.map((post) => (
-        <div key={post.id}>{post.name}</div>
-      ))}
-      {mockLogos.map((logo) => (
-        <div key={logo.id} className="m-4">
-          <img src={logo.url} alt={`Logo ${logo.id}`} />
+      {images.map((image) => (
+        <div key={image.id} className="m-4">
+          <img src={image.url} alt={`Logo ${image.id}`} />
         </div>
       ))}
     </main>
