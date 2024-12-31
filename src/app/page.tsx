e300577968b19877e58328f4dfcd4370b68b2361
Andrew Scott Pitblado <apitblado@gmail.com>
2024-12-31 12:57:55 -0500

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 import { db } from "~/server/db";
-
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function logos() {
   const images = await db.query.images.findMany();
   return (
-    <main className="flex min-h-screen flex-wrap items-center justify-center">
+    <div className="flex flex-wrap gap-4">
       {images.map((image) => (
         <div key={image.id} className="m-4">
           <img src={image.url} alt={`Logo ${image.id}`} />
         </div>
       ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-wrap items-center justify-center">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in to play the Logo Game.
+        </div>
+      </SignedOut>
+      <SignedIn>{logos()}</SignedIn>
     </main>
   );
 }
