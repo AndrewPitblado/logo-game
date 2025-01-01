1c10baa84cc8015803a9369e5dfd4ce273578a48
Andrew Scott Pitblado <apitblado@gmail.com>
2025-01-01 13:04:37 -0500

    Added image upload

--- src/app/_components/topNav.tsx
+++ src/app/_components/topNav.tsx
@@ -1,15 +1,25 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+"use client";
 
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../../utils/uploadthing";
+import { useRouter } from "next/navigation";
 export function TopNav() {
+  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b bg-gray-800 p-4 font-semibold text-white">
       <div className="text-2xl">Logo Game</div>
       <div className="text-2xl">Score: 0</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
